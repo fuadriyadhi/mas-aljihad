9cb95d69d3c4a58dc6614575d956afec263def3c
fuadriyadhi <fuadriyadhi678@gmail.com>
2025-05-23 14:12:06 +0700

    upload new photos and make a btn_more_less

--- script.js
+++ script.js
@@ -24,5 +24,31 @@ menuToggle.addEventListener("click", () => {
   navbarMenu.classList.toggle("active");
 });
 
-
 // masAljihad-06
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll("#galeri-container .col-md-4");
+  const showMoreBtn = document.getElementById("show-more-btn");
+  const showLessBtn = document.getElementById("show-less-btn");
+
+  function showInitialCards() {
+    cards.forEach((card, index) => {
+      card.style.display = index > 5 ? "none" : "block";
+    });
+    showMoreBtn.classList.remove("d-none");
+    showLessBtn.classList.add("d-none");
+  }
+
+  showInitialCards(); // tampilkan hanya 6 pertama saat awal
+
+  showMoreBtn.addEventListener("click", function () {
+    cards.forEach(card => {
+      card.style.display = "block";
+    });
+    showMoreBtn.classList.add("d-none");
+    showLessBtn.classList.remove("d-none");
+  });
+
+  showLessBtn.addEventListener("click", function () {
+    showInitialCards();
+  });
+});
